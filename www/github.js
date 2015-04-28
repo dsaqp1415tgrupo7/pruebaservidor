2360cd8fd741ffb9bf41403467261d6db14c494b
raulgonzalez83 <raul.gonzalez83@gmail.com>
2015-04-28 18:56:21 +0200

    Se añade el html y el js

---
+++ www/github.js
@@ -0,0 +1,37 @@
+var API_BASE_URL = "http://localhost:8080/entrega.api/myresource";
+
+
+/*
+Details about repository of GitHub API 
+https://developer.github.com/v3/repos/
+*/
+
+$("#button_got_it").click(function(e) {
+	e.preventDefault();
+	getMensaje();
+});
+
+
+
+
+function getMensaje() {
+	var url = API_BASE_URL;
+	$("#result").text('');
+
+	$.ajax({
+		url : url,
+		type : 'GET',
+		crossDomain : true
+	}).done(function(data, status, jqxhr) {
+
+				var repo = data;
+
+				$("#result").text(repo);
+				
+				
+			}).fail(function() {
+				$('<div class="alert alert-danger"> <strong>Oh!</strong> Repository not found </div>').appendTo($("#result"));
+	});
+
+}
+
